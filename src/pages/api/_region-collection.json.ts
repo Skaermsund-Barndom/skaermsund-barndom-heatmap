0048f961fb25cbfbe60f4529deec8fa81681f9fe
Eigil Nikolajsen <eigi0088@gmail.com>
2025-04-30 15:31:01 +0200

    fix: Data fetching and api routes

---
+++ src/pages/api/_region-collection.json.ts
@@ -0,0 +1,21 @@
+import { getRegionCollection } from "@/scripts/collections";
+import { tryCatch } from "@/scripts/try-catch";
+import type { APIRoute } from "astro";
+
+export const GET: APIRoute = async () => {
+	const { data, error } = await tryCatch(getRegionCollection);
+
+	if (error || !data) {
+		return new Response(JSON.stringify({ error }), {
+			status: 500,
+		});
+	}
+
+	return new Response(JSON.stringify(data), {
+		status: 200,
+		headers: {
+			"Content-Type": "application/json",
+			"Cache-Control": "public, max-age=31536000",
+		},
+	});
+};
---
+++ src/pages/api/_school-collection.json.ts
@@ -0,0 +1,21 @@
+import { getSchoolCollection } from "@/scripts/collections";
+import { tryCatch } from "@/scripts/try-catch";
+import type { APIRoute } from "astro";
+
+export const GET: APIRoute = async () => {
+	const { data, error } = await tryCatch(getSchoolCollection);
+
+	if (error || !data) {
+		return new Response(JSON.stringify({ error }), {
+			status: 500,
+		});
+	}
+
+	return new Response(JSON.stringify(data), {
+		status: 200,
+		headers: {
+			"Content-Type": "application/json",
+			"Cache-Control": "public, max-age=31536000",
+		},
+	});
+};
